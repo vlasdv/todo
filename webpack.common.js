6472a5203f16e967730dc72ab0c6cecc03751709
Dmitrii Vlasov <vlasdev@icloud.com>
2024-11-21 21:37:10 +0100

    Install webpack

---
+++ webpack.common.js
@@ -0,0 +1,30 @@
+// webpack.common.js
+
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
